a5885f02ad88bff2c8e322dc83279284eb2cbb54
Giorgio Zanchetti <42031013+Girg-Z@users.noreply.github.com>
2020-11-22 21:06:27 +0100

    Update mysql_secure_installation.sh

--- lib/mysql_secure_installation.sh
+++ lib/mysql_secure_installation.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-echo "SET PASSWORD FOR root@localhost = PASSWORD('$1');"
+#Set root password as first argument
+#Delete anonymous users
+#Disallow root login remotely
+#Remove test database
 
 mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$1');"
 mysql -e "DELETE FROM mysql.user WHERE User='';"
 mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "FLUSH PRIVILEGES;"
